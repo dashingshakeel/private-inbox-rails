0f0688b360594d0f5fa98b9ff4ed40ea47e9ef89
dashingshakeel <shkeelrauf@gmail.com>
2017-03-17 15:41:04 +0500

    Added mailboxer db and routes for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
