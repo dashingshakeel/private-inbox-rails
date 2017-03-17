72296285d15901b96634b0485c27d43d8647fa54
dashingshakeel <shkeelrauf@gmail.com>
2017-03-17 17:06:28 +0500

    All the messages sent code added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :mailbox, :conversation
+  def conversation
+    @conversation ||= mailbox.conversations.find(params[:id])
+  end
+  def mailbox
+    @mailbox ||= current_user.mailbox
+    
+  end
   protected
 
   def configure_permitted_parameters
